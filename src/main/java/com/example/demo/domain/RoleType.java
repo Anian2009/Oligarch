f28842624b962abed078683247c7434f82dea70a
victor <victorpaul.war@gmail.com>
2019-01-15 19:25:44 +0200

    removed everything for review

--- src/main/java/com/example/demo/domain/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.domain;
-
-public enum RoleType {
-    ADMIN,
-    USER
-}
--- src/main/java/com/example/demo/repository/FabricsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.Fabrics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FabricsRepository extends JpaRepository<Fabrics, Long> {
-    Fabrics findById(Integer id);
-
-}
