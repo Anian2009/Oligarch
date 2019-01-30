d9ff93a4d40511cdb165e213785ed7fdfd24f1e9
Anian2009 <Anian2009@ukr.net>
2019-01-30 14:38:08 +0200

    add favicon

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-//        app.listen(process.env.PORT);
     }
 
 }
